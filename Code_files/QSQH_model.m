cf4f964a5da85a04bc7194b5e6191ea03081040d
Yunjiu Yang <yunjiu.yang20@imperial.ac.uk>
2023-11-15 15:56:27 +0000

    Change y_ref interpolation

--- Code_files/QSQH_model.m
+++ Code_files/QSQH_model.m
@@ -1,14 +1,16 @@
 %% Load the all the input data
 load(dir_vel_tilde);
-load y.mat;
+load(dir_y_ref,'y.mat');
 load(dir_u_tauL_stats,'u_tauL_stats.mat');
 
 %% Set up the uniform grid
 
 % 1D grid in x,z,y directions
-x_plus_ref = 0:12:300;
-z_plus_ref = 0:6:300;
-y_plus_ref = y(1:ymax).';
+x_plus_ref = 0:12:xmax;
+z_plus_ref = 0:6:zmax;
+
+[val,id] = min(abs(ymax - y));
+y_plus_ref = y(1:id).';
 
 % 3D grid
 [Xref,Zref,Yref] = ndgrid(x_plus_ref,z_plus_ref,y_plus_ref);
