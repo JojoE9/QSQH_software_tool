1eb9062e629e342aedc133bc2ab38a44689b6a57
Yunjiu Yang <yunjiu.yang20@imperial.ac.uk>
2024-02-07 06:02:15 +0800

    Interface corrected

--- Python/Interface.py
+++ Python/Interface.py
@@ -1,13 +1,32 @@
-import os
-
 # Interface of QSQH tool
 
-# Clear the screen
-os.system('clear')  # On Windows, you can use 'cls' instead of 'clear'
+import os
 
-# User input parameters
-with open('InputParameters.txt', 'r') as file:
-    lines = file.readlines()
+# Get the current working directory
+current_directory = os.getcwd() 
+
+# Specify the subfolder name and the filename
+InputFile_name = 'InputParameters.txt'     # Replace with your text file name
+
+# Construct the absolute path to the text file
+InputFile_path = os.path.join(current_directory, file_name)
+
+# Check if the file exists before attempting to open it
+if os.path.exists(file_path):
+    try:
+        # Open and read the file
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+
+        # Do something with the file content
+        print(file_content)
+
+    except FileNotFoundError:
+        print(f"The file '{file_path}' was not found.")
+    except IOError as e:
+        print(f"An error occurred while reading the file: {e}")
+else:
+    print(f"The file '{file_path}' does not exist.")
 
 # Execute the code
-exec(open('Excute_code.py').read())
+exec(open('Execute_code.py').read())
