1bc8679e0bb709dd12050e2b2ac97c5379c8b594
Yunjiu Yang <yunjiu.yang20@imperial.ac.uk>
2024-02-07 07:19:07 +0800

    Interface corrected

--- Python/Interface.py
+++ Python/Interface.py
@@ -1,15 +1,16 @@
 # Interface of QSQH tool
 
 import os
+import subprocess
 
 # Get the current working directory
 current_directory = os.getcwd() 
 
-# Specify the subfolder name and the filename
-InputFile_name = 'InputParameters.txt'     # Replace with your text file name
+# Specify the the input filename
+InputFile_name = 'InputParameters.txt' 
 
-# Construct the absolute path to the text file
-InputFile_path = os.path.join(current_directory, file_name)
+# Construct the absolute path to the InputParameters.txt
+InputFile_path = os.path.join(current_directory, InputFile_name)
 
 # Check if the file exists before attempting to open it
 if os.path.exists(InputFile_path):
@@ -28,5 +29,14 @@ if os.path.exists(InputFile_path):
 else:
     print(f"The file '{InputFile_path}' does not exist.")
 
-# Execute the code
-exec(open('Execute_code.py').read())
+# Specify the the execution filename
+Execution_name = 'Execution_code.py' 
+
+# Construct the absolute path to the Execution_code.py
+Execution_path = os.path.join(current_directory,Execution_name)
+
+try:
+    # Run the specified Python script
+    subprocess.run(["python", Execution_path])
+except Exception as e:
+    print(f"An error occurred: {e}")
