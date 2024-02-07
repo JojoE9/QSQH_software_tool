b83699e99871866ec64e5dfc68769a46a3e75849
Yunjiu Yang <yunjiu.yang20@imperial.ac.uk>
2024-02-07 15:17:45 +0800

    Execution corrected

--- Python/Execute_code.py
+++ Python/Execute_code.py
@@ -1,20 +1,18 @@
-import os
-
 # Extract values after comma ',' from the 'lines' list
-InputParameters = lines.split(',')[1:]
+InputParameters = [line.split(',')[1] for line in lines]
 
 # Convert strings to floating-point numbers
 xmax = float(InputParameters[2])
 zmax = float(InputParameters[3])
 
-# Get the current working directory and file separator
-workdir = os.getcwd()
-file_separator = InputParameters[9]
-
 if xmax == 0 and zmax == 0:
-    script_path = os.path.join(workdir, 'QSQH_model', 'QSQH_model_1D')
+    script_path = os.path.join(current_directory, 'QSQH_model', 'QSQH_model_1D')
 else:
-    script_path = os.path.join(workdir, 'QSQH_model', 'QSQH_model_3D')
+    script_path = os.path.join(current_directory 'QSQH_model', 'QSQH_model_3D')
 
 # Execute the corresponding script
-os.system(script_path)
+try:
+    # Run the specified Python script
+    subprocess.run(["python", script_path])
+except Exception as e:
+    print(f"An error occurred: {e}")
