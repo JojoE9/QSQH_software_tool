21b286085da4bbafd4decab6d6135c4ec9931051
Yunjiu Yang <yunjiu.yang20@imperial.ac.uk>
2025-06-05 17:45:30 +0800

    Combine Interface and Excute_code, add more details in structure

--- model/Interface.m
+++ model/Interface.m
@@ -6,4 +6,19 @@ clc
 lines = readlines('InputParameters.txt');
 
 % Excute the code
-run('Excute_code');
+
+InputParameters = extractAfter(lines(:),',');
+
+xmax = str2double(InputParameters(3));
+zmax = str2double(InputParameters(4));
+
+workdir = pwd;
+file_separator = convertStringsToChars(InputParameters(10));
+
+if xmax == 0 && zmax == 0
+    run([workdir,file_separator,'QSQH_model',file_separator,...
+        'QSQH_model_1D'])
+else
+    run([workdir,file_separator,'QSQH_model',file_separator,...
+        'QSQH_model_3D'])
+end
